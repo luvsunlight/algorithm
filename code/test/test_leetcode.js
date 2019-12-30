d5f119b4c793bb15785b2918dab4d4aac6924a2a
luvsunlight <516060659@qq.com>
2019-12-30 20:03:41 +0800

    add test

--- code/test/test_leetcode.js
+++ code/test/test_leetcode.js
@@ -1,59 +1,36 @@
-/**
- * @param {character[][]} board
- * @return {boolean}
- */
-var isValidSudoku = function(board) {
-	for (let i = 0; i < 9; i++) {
-		let hashmap = {}
-		for (let j = 0; j < 9; j++) {
-			if (board[i][j] === ".") continue
-			if (hashmap[board[i][j]]) {
-				return false
-			} else {
-				hashmap[board[i][j]] = true
-			}
-		}
-	}
-	for (let i = 0; i < 9; i++) {
-		let hashmap = {}
-		for (let j = 0; j < 9; j++) {
-			if (board[j][i] === ".") continue
-			if (hashmap[board[j][i]]) {
-				return false
-			} else {
-				hashmap[board[j][i]] = true
-			}
-		}
-	}
-	for (let i = 0; i < 9; i += 3) {
-		for (let j = 0; j < 9; j += 3) {
-			let hashmap = {}
-			for (let m = i; m < i + 3; m++) {
-				for (let n = j; n < j + 3; n++) {
-					if (board[m][n] === ".") continue
-					if (hashmap[board[m][n]]) {
-						return false
-					} else {
-						hashmap[board[m][n]] = true
-					}
-				}
-			}
-		}
-	}
+// console.log(1)
+// setTimeout(() => {
+// 	console.log(2)
+// }, 0)
+// Promise.resolve().then(() => {
+// 	console.log(3)
+// 	Promise.resolve().then(() => {
+// 		console.log(4)
+// 	})
+// })
 
-	return true
-}
+console.log("sync1")
 
-console.log(
-	isValidSudoku([
-		[".", ".", ".", ".", "5", ".", ".", "1", "."],
-		[".", "4", ".", "3", ".", ".", ".", ".", "."],
-		[".", ".", ".", ".", ".", "3", ".", ".", "1"],
-		["8", ".", ".", ".", ".", ".", ".", "2", "."],
-		[".", ".", "2", ".", "7", ".", ".", ".", "."],
-		[".", "1", "5", ".", ".", ".", ".", ".", "."],
-		[".", ".", ".", ".", ".", "2", ".", ".", "."],
-		[".", "2", ".", "9", ".", ".", ".", ".", "."],
-		[".", ".", "4", ".", ".", ".", ".", ".", "."]
-	])
-)
+setTimeout(function() {
+	console.log("setTimeout1")
+}, 0)
+
+var promise = new Promise(function(resolve, reject) {
+	setTimeout(function() {
+		console.log("setTimeoutPromise")
+	}, 0)
+	console.log("promise")
+	resolve()
+})
+
+promise.then(() => {
+	console.log("pro_then")
+	setTimeout(() => {
+		console.log("pro_timeout")
+	}, 0)
+})
+
+setTimeout(function() {
+	console.log("last_setTimeout")
+}, 0)
+console.log("sync2")
